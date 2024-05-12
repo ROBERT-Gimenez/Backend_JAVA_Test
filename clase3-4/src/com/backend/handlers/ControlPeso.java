fde2f0d2a14f85b8874df72c1ff1388321244fab
ROBERT-Gimenez <robertgim37@gmail.com>
2024-05-11 21:10:02 -0300

    ejercicios de mesa de la clase 3 subida

---
+++ clase3-4/src/com/backend/handlers/ControlPeso.java
@@ -0,0 +1,2 @@
+package com.backend.handlers;public class ControlPeso {
+}
---
+++ clase3-4/src/com/backend/test/ControlCalidadTest.java
@@ -0,0 +1,8 @@
+package com.backend.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControlCalidadTestTest {
+    
+
+}
